1053fb2347ca354eabd6f9cdf2f06ea481a34eb0
calebfergie <yonohabloespanol@gmail.com>
2021-05-31 17:40:32 -0400

    init baybeee

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+
+// dotenv baby
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.set('view engine', 'ejs')
+// res.render(view, locals)
+
+
+MongoClient.connect("mongodb+srv://"+process.env.MONGOUN+":"+ process.env.MONGOPW + ".HIRT@huecluster0.i6cxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useUnifiedTopology: true
+  })
+  .then(client => {
+    console.log('Connected to Mongo Database')
+    const db = client.db('HueData')
+    const HueDataSensor1 = db.collection('HueDataSensor1')
+    app.get('/', (req, res) => {
+      db.collection('HueDataSensor1').find().toArray()
+        .then(results => {
+          res.render('index.ejs', {
+            HueDataSensor1: results
+          })
+        })
+        .catch( /* ... */ )
+
+    })
+
+  })
+  .catch(error => console.error(error))
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
